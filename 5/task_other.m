3359b1552b75a264a86cfa632c9ea314496ceee9
Matthew Du <crepuscian@foxmail.com>
2018-11-18 14:33:17 +0000

    finished initial

--- 5/task_other.m
+++ 5/task_other.m
@@ -1,3 +1,17 @@
 load('procTime52.mat');
-s_x = var(processingSize);
-s_y = var(processingTime);
+n = 300;
+
+s_x = sqrt(var(processingSize));
+s_y = sqrt(var(processingTime));
+
+x_avg = mean(processingSize);
+y_avg = mean(processingTime);
+
+sum = 0;
+for index = 1 : n
+    sum = sum + (processingSize(index) - x_avg).*(processingTime(index) - y_avg);
+end
+
+r_xy = ((1 ./ n) * sum) ./ (s_x .* s_y);
+
+r_xy
